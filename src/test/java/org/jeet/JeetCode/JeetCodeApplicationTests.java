43b303776fc877e691534806710f8d644699a46d
keshri-prasanjeet <keshriprasanjeet2@outlook.com>
2023-12-18 18:56:48 +0530

    this one does it hopefully

---
+++ src/test/java/org/jeet/JeetCode/JeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeet.JeetCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeetCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
